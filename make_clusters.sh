169441cbc0125d6d25b63ce3bb01825269efb324
Juan Eiros <juaneiros@hotmail.com>
2015-07-12 18:51:58 +0100

    trying2s

--- make_clusters.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for count in {3..9}
-do
-cluster=$((2**$count))
-msmb KCenters -i ../05_Prod_comphowcut_000-050ns_run3.nc -o assignments/CA_run3_KCenters_$cluster -t assignments/CA_run3_KCenters_$cluster --metric "rmsd" --top ../../comphowcut_c_20Anowat.pdb --n_clusters $cluster
-done
-
