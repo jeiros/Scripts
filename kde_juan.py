93fb4476e8882bf04dded687f0beccdedf0a91a5
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2015-07-12 18:16:04 +0100

    PEP8'in
    Practiced with SublimeText3 to make the python scripts PEP8 complaint

--- kde_juan.py
+++ kde_juan.py
@@ -3,8 +3,8 @@
 RUN THE SCRIPT AS 
         $ python ./kde_juan.py myevecs000-050ns_run1.dat myevecs000-050ns_run2.dat
 """
-import numpy as np 
-import pylab as pl 
+import numpy as np
+import pylab as pl
 from scipy import stats
 from scipy import interpolate
 import matplotlib.pyplot as plt
@@ -20,20 +20,21 @@ def parse_data(files):
     maxx = 0
     maxy = 0
     for file in sys.argv[1:]:
-        f_colx = np.loadtxt(file, usecols=(1,), skiprows = 1)
-        f_coly = np.loadtxt(file, usecols=(2,), skiprows = 1)
+        f_colx = np.loadtxt(file, usecols=(1,), skiprows=1)
+        f_coly = np.loadtxt(file, usecols=(2,), skiprows=1)
         minx = min(f_colx.min(), minx)
         maxx = max(f_colx.max(), maxx)
         miny = min(f_coly.min(), miny)
         maxy = max(f_coly.max(), maxy)
         data.append([f_colx, f_coly])
 
-    # Added +-10 to the edges so the KDE is better seen (if not sometimes its cut for the data that is in the limits of the plot)    
+    # Added +-10 to the edges so the KDE is better seen (if not sometimes its cut for the data that is in the limits of the plot)
     minx = minx - 10
     maxx = maxx + 10
     miny = miny - 10
     maxy = maxy + 10
     return (data, minx, maxx, miny, maxy)
+
 
 def generate_kde(data):
     """ Returns a list called kernels with the kernel density estimator for each value stored in the data list"""
@@ -42,6 +43,7 @@ def generate_kde(data):
         values = np.vstack((data[i][0], data[i][1]))
         kernels.append(stats.gaussian_kde(values))
     return kernels
+
 
 def generate_shape(kernels, minx, maxx, miny, maxy):
     """Returns a list called shapes that stores the needed values to plot the shape of the KDE
@@ -56,32 +58,35 @@ def generate_shape(kernels, minx, maxx, miny, maxy):
 
     return shapes
 
+
 def generate_title_from_file(title):
     """This function removes the "myevecs" string and ".dat" from the files to create the title
      The input files should look like "myevcs000-050ns_run1.dat" and the title that is created is 000-050ns_run1"""
-    return title.replace("myevecs","").replace(".dat","")
+    return title.replace("myevecs", "").replace(".dat", "")
+
 
 def plot(shapes, data, minx, maxx, miny, maxy):
     figs = []
     for i in range(0, len(shapes)):
         figs.append(plt.figure())
-        ax = figs[i].add_subplot(111)   
-        figs[i].suptitle(generate_title_from_file(sys.argv[i+1]), fontsize = 14)
+        ax = figs[i].add_subplot(111)
+        figs[i].suptitle(generate_title_from_file(sys.argv[i + 1]), fontsize=14)
         ax.imshow(np.rot90(shapes[i]), cmap=plt.cm.gist_earth_r, extent=[minx, maxx, miny, maxy])
         ax.set_xlabel('PCA1')
         ax.set_ylabel('PCA2')
         ax.set_xlim([minx, maxx])
         ax.set_ylim([miny, maxy])
-        #ax.plot(data[i][0], data[i][1], 'k.', markersize=2)             # Comment or uncomment this line to plot the dots
-        figs[i].savefig(sys.argv[i+1].replace(".dat","") + ".png")      # Comment or uncomment this line if we want to save the plots as a .png file
-                                                                        #   with the name "myevecs000-050ns_run1.png"
-    plt.show()
+        # ax.plot(data[i][0], data[i][1], 'k.', markersize=2)             # Comment or uncomment this line to plot the dots
+        figs[i].savefig(sys.argv[i + 1].replace(".dat", "") + ".png")      # Comment or uncomment this line if we want to save the plots as a .png file
+        #   with the name "myevecs000-050ns_run1.png"
+        plt.show()
+
 
 def main():
     data, minx, maxx, miny, maxy = parse_data(sys.argv)
     kernels = generate_kde(data)
     shapes = generate_shape(kernels, minx, maxx, miny, maxy)
     plot(shapes, data, minx, maxx, miny, maxy)
-    
+
 if __name__ == "__main__":
     main()
--- pca_analysis.py
+++ pca_analysis.py
@@ -12,45 +12,44 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from itertools import combinations
 
+
 def load_Trajs(names, topology):
     filenames = sorted(glob.glob(names))
-    trajectories = md.load(filenames, top = topology)
+    trajectories = md.load(filenames, top=topology)
     return trajectories
 
-def plot_PCA1(trajectory):
-	pca1 = PCA(n_components = 2)
-	trajectory.superpose(trajectory, 0)
-	reduced_cartesian = pca1.fit_transform(trajectory.xyz.reshape(trajectory.n_frames, trajectory.n_atoms * 3))
-	plt.figure()
-	plt.scatter(reduced_cartesian[:, 0], reduced_cartesian[:,1], marker = 'x', c = trajectory.time)
-	plt.xlabel('PC1')
-	plt.ylabel('PC2')
-	plt.title('Cartesian coordinate PCA')
-	cbar = plt.colorbar()
-	cbar.set_label('Time [ps]')
 
+def plot_PCA1(trajectory):
+    pca1 = PCA(n_components=2)
+    trajectory.superpose(trajectory, 0)
+    reduced_cartesian = pca1.fit_transform(trajectory.xyz.reshape(trajectory.n_frames, trajectory.n_atoms * 3))
+    plt.figure()
+    plt.scatter(reduced_cartesian[:, 0], reduced_cartesian[:, 1], marker='x', c=trajectory.time)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.title('Cartesian coordinate PCA')
+    cbar = plt.colorbar()
+    cbar.set_label('Time [ps]')
 
 
 def plot_PCA2(trajectory):
-	pca2 = PCA(n_components=2)
-	atom_pairs = list(combinations(range(trajectory.n_atoms), 2))
-	pairwise_distances = md.geometry.compute_distances(trajectory, atom_pairs)
-	reduced_distances = pca2.fit_transform(pairwise_distances)
-	plt.figure()
-	plt.scatter(reduced_distances[:, 0], reduced_distances[:,1], marker='x', c=trajectory.time)
-	plt.xlabel('PC1')
-	plt.ylabel('PC2')
-	plt.title('Pairwise distance PCA: alanine dipeptide')
-	cbar = plt.colorbar()
-	cbar.set_label('Time [ps]')
-
-
+    pca2 = PCA(n_components=2)
+    atom_pairs = list(combinations(range(trajectory.n_atoms), 2))
+    pairwise_distances = md.geometry.compute_distances(trajectory, atom_pairs)
+    reduced_distances = pca2.fit_transform(pairwise_distances)
+    plt.figure()
+    plt.scatter(reduced_distances[:, 0], reduced_distances[:, 1], marker='x', c=trajectory.time)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.title('Pairwise distance PCA: alanine dipeptide')
+    cbar = plt.colorbar()
+    cbar.set_label('Time [ps]')
 
 
 def main():
-	trajectory = load_Trajs(sys.argv[1], sys.argv[2])
-	plot1 = plot_PCA1(trajectory)
-	#plot2 = plot_PCA2(trajectory)
+    trajectory = load_Trajs(sys.argv[1], sys.argv[2])
+    plot1 = plot_PCA1(trajectory)
+    # plot2 = plot_PCA2(trajectory)
 
 if __name__ == "__main__":
     import sys
