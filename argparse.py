8a0897695e310e10c27fcb5a21684295ef9a57a8
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2015-07-13 11:57:45 +0100

    Got rid of argparse.py

--- argparse.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.parse_args()
--- kde_juan.py
+++ kde_juan.py
@@ -12,11 +12,11 @@ import sys
 import argparse
 
 
-parser = argparse.ArgumentParser(usage="{} file1 [file2 [file3 [... ] ] ]".format(prog),
-                                 epilog="Plot a series of KDE plots for every\
-                                 evecs file passed as argument. Forces\
-                                 the axis to be the same for all of them for\
-                                 easier comparison")
+parser = argparse.ArgumentParser(usage="{} file1 [file2 [file3 [... ] ] ]".
+                                 format(sys.argv[0]), epilog="Plot a series of KDE \
+                                 plots for every evecs file passed as \
+                                 argument. Forces the axis to be the same for \
+                                 all of them for easier comparison")
 
 parser.parse_args()
 
