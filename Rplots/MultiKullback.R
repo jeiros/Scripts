5fccd998e057e8b563094b17744f73d136242331
Juan Eiros <juaneiros@hotmail.com>
2015-07-14 19:33:06 +0100

    Reduced the y axis to better visualize the KLD on the sub 1 values

--- Rplots/MultiKullback.R
+++ Rplots/MultiKullback.R
@@ -21,13 +21,13 @@ d1 <- melt(pc1, id.vars = "Frame")
 d2 <- melt(pc2, id.vars = "Frame")
 d3 <- melt(pc3, id.vars = "Frame")
 
-p1 <- ggplot(d1, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("Principal Component 1") + scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black"))
+p1 <- ggplot(d1, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("PC1") + scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black")) + scale_y_continuous(limits=c(0,1)) + scale_x_continuous(limits=c(10000,34000))
 
-p2 <- ggplot(d2, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("Principal Component 1") + scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black"))
+p2 <- ggplot(d2, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("PC2") + scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black")) + scale_y_continuous(limits=c(0,1)) + scale_x_continuous(limits=c(10000,34000))
 
-p3 <- ggplot(d3, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("Principal Component 1") + scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black"))
+p3 <- ggplot(d3, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("PC3") + scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black")) + scale_y_continuous(limits=c(0,1)) + scale_x_continuous(limits=c(10000,34000))
 
 ppi=900
-png("KL-PC.png", width = 8*ppi, height = 8*ppi, res = ppi)
+png("KL-PC_reduced_y_scale.png", width = 8*ppi, height = 8*ppi, res = ppi)
 multiplot(p1,p2,p3)
 dev.off()
