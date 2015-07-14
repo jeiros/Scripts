fae2d7bb3c982d823188243a40551d468ebd49f1
Juan Eiros <juaneiros@hotmail.com>
2015-07-14 16:55:10 +0100

    Modified pca_enhanced.sh to be able to use it more generally. It should be used directly in the directory where the trajectories and the prmtop is. Now you can choose the name directly

--- AnalysisCpptraj/pca_enhanced.sh
+++ AnalysisCpptraj/pca_enhanced.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-
-WORKDIR=$PWD
-
-run=$1 		  # Can be run1 run2 run3 run4 ...
-simtime=$2		  # Can be 000-050, 000-0500, 100-150 ...
-cluster=$3        # Can be CTnI_hmr CTnI_runs CTnT_hmr CTnT_runs
-phosphotype=$4    # Can be S1P or SEP
-
-
-WORKDIR=$PWD
-printf "\nCurrent directory is %s\n" $WORKDIR
-DESTINATION=${WORKDIR}/${cluster}/${run}/${phosphotype}
-
-printf "The destination directory is %s\n\n" $DESTINATION
-
-if [ -d "$DESTINATION" ]; then 
-	printf "The directory exists\n\n"
-else
-	printf "The destination directory does not exist\n"
-	printf "\nPlease check the arguments
---------------------------------
-Run:\t\t\t%s\nSim:\t\t\t%s\nCluster:\t\t%s\nPhosphorylation type:\t%s
---------------------------------\n" ${run} ${simtime} ${cluster} ${phosphotype}
-	printf "Exiting now...\n\n"
-	exit 1
-fi
+# Advanced analysis of PCA with cpptraj
+# Must run on the directory where the trajectories
+# and the prmtop are. 
 
 
 
-cd ${DESTINATION}
+
+
+simtime=$1		  # Can be 000-050, 000-0500, 100-150 ...
+name=$2        # Can be CTnI_hmr CTnI_runs CTnT_hmr CTnT_runs with _run1 _run2 ...
+								# Example: CTnI_hmr-run3-S1P
+								# Example: WT-run3
+
+prmtop=$3
+trajs=$4
+
 
 
 cpptraj <<- EOF
-	parm ./repstr.c0_phosS1P_nowat.prmtop
-	trajin ./05_Prod*.nc
+	parm ./${prmtop}
+	trajin ./${trajs}
 	# Fit to first frame, create average structure, save coordinates
 	rms first
 	average crdset average_structure
@@ -48,38 +34,38 @@ cpptraj <<- EOF
 
 	#Diagonalize the cov matrix and get first 3 eigenvectors
 	runanalysis diagmatrix matrix_covar out \
-		${DESTINATION}/evecs-ca_${cluster}.${simtime}ns.dat vecs 200 name \
-		myEvecs @CA,C,O,N,H
+		./evecs-ca_${name}.${simtime}ns.dat vecs 200 name \
+		myEvecs
 
 	# Project fit and saved coordinates along eigenvectors
 	crdaction loaded_trajs projection PROJECT modes myEvecs beg 1 end 3 \
-		@CA,C,O,N,H out ${DESTINATION}/myevecs_${cluster}.${simtime}ns.dat
+		@CA,C,O,N,H out ./myevecs_${name}.${simtime}ns.dat
 
 	# Make normalized histogram of the 3 calculated projections
 	hist PROJECT:1 bins 100 out \
-		${DESTINATION}/${cluster}_${simtime}-hist.dat norm name PROJECT-1
+		./${name}_${simtime}-hist.dat norm name PROJECT-1
 
 	hist PROJECT:2 bins 100 out \
-		${DESTINATION}/${cluster}_${simtime}-hist.dat norm name PROJECT-2
+		./${name}_${simtime}-hist.dat norm name PROJECT-2
 
 	hist PROJECT:3 bins 100 out \
-		${DESTINATION}/${cluster}_${simtime}-hist.dat norm name PROJECT-3
+		./${name}_${simtime}-hist.dat norm name PROJECT-3
 
 	# Run and clear
 	run
 	clear all
 
 	# Read the file with the eigenvectors
-	readdata ${DESTINATION}/evecs-ca_${cluster}.${simtime}ns.dat \
+	readdata ./evecs-ca_${name}.${simtime}ns.dat \
 		name Evecs
 
 	# Create a topology for the backbone only
-	parm ${DESTINATION}/repstr.c0_phosS1P_nowat.prmtop
+	parm ./${prmtop}
 	parmstrip !(@CA,C,O,N,H)
-	parmwrite out ${DESTINATION}/repstr.c0_phosS1P_nowat_striped.prmtop 
+	parmwrite out ./repstr.c0_phosS1P_nowat_backbone.prmtop 
 
 	#Create a .nc trajectory with the modes of motion of the 1st PC
-	runanalysis modes name Evecs trajout ${DESTINATION}/${cluster}_${simtime}_1PCA.nc \
+	runanalysis modes name Evecs trajout ./${name}_${simtime}_1PCA.nc \
 		pcmin -100 pcmax 100 tmode 1 trajoutmask @CA,C,O,N,H trajoutfmt netcdf
 
 EOF
