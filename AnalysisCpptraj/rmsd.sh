8bdd93094e85f0eba701a2f927129f481e72c173
Juan Eiros <juaneiros@hotmail.com>
2015-07-15 17:36:45 +0100

    Made rmsd.sh more general (give the trajectories manually)

--- AnalysisCpptraj/rmsd.sh
+++ AnalysisCpptraj/rmsd.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 
-WORKDIR=$PWD
 
-cluster=$1
+simtime=$1		  # Can be 000-050, 000-0500, 100-150 ...
+name=$2        # Can be CTnI_hmr CTnI_runs CTnT_hmr CTnT_runs with _run1 _run2 ...
+								# Example: CTnI_hmr-run3-S1P
+								# Example: WT-run3
+prmtop=$3
+trajs=$4
 
-
-simtime=000-250
-
-cd ${WORKDIR}/${cluster}/run1/S1P/
 
 cpptraj <<- EOF
-	parm ./repstr.c0_phosS1P_nowat.prmtop
-	trajin ./05_Prod*.nc
-	rms rmsd @CA,C,O,N,H first out ${WORKDIR}/rmsd_${cluster}.${simtime}ns.dat mass
+	parm ./${prmtop}
+	trajin ./${trajs}
+	rms rmsd @CA,C,O,N,H first out ./rmsd_${name}.${simtime}ns.agr mass
 	run
 EOF
-cd $WORKDIR
