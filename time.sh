9a605192b72d49c7a5c685c48ed6eeea65ee8326
Juan Eiros <juaneiros@hotmail.com>
2015-08-25 14:05:38 +0100

    blah

---
+++ time.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+awk '{print $1*0.02,$2}' 420-D65OD1_mhr7.dat > 420-D65OD1_mhr72.dat
+mv 420-D65OD1_mhr72.dat 420-D65OD1_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-D65OD2_mhr7.dat > 420-D65OD2_mhr72.dat
+mv 420-D65OD2_mhr72.dat 420-D65OD2_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-D67OD1_mhr7.dat > 420-D67OD1_mhr72.dat
+mv 420-D67OD1_mhr72.dat 420-D67OD1_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-D67OD2_mhr7.dat > 420-D67OD2_mhr72.dat
+mv 420-D67OD2_mhr72.dat 420-D67OD2_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-D73OD1_mhr7.dat > 420-D73OD1_mhr72.dat
+mv 420-D73OD1_mhr72.dat 420-D73OD1_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-D73OD2_mhr7.dat > 420-D73OD2_mhr72.dat
+mv 420-D73OD2_mhr72.dat 420-D73OD2_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-E76OE1_mhr7.dat > 420-E76OE1_mhr72.dat
+mv 420-E76OE1_mhr72.dat 420-E76OE1_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-E76OE2_mhr7.dat > 420-E76OE2_mhr72.dat
+mv 420-E76OE2_mhr72.dat 420-E76OE2_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-S69OG_mhr7.dat > 420-S69OG_mhr72.dat
+mv 420-S69OG_mhr72.dat 420-S69OG_mhr7.dat
+
+awk '{print $1*0.02,$2}' 420-T71OG1_mhr7.dat > 420-T71OG1_mhr72.dat
+mv 420-T71OG1_mhr72.dat 420-T71OG1_mhr7.dat
+
+awk '{print $1*0.02,$2}' 271-272_420_mhr7.dat > 271-272_420_mhr72.dat
+mv 271-272_420_mhr72.dat 271-272_420_mhr7.dat
+
+awk '{print $1*0.02,$2}' 284-286_420_mhr7.dat > 284-286_420_mhr72.dat
+mv 284-286_420_mhr72.dat 284-286_420_mhr7.dat
+
+awk '{print $1*0.02,$2}' 30-38_271-272_mhr7.dat > 30-38_271-272_mhr72.dat
+mv 30-38_271-272_mhr72.dat 30-38_271-272_mhr7.dat
+
+awk '{print $1*0.02,$2}' K233-421_mhr7.dat > K233-421_mhr72.dat
+mv K233-421_mhr72.dat K233-421_mhr7.dat
+
+awk '{print $1*0.02,$2}' K233-422_mhr7.dat > K233-422_mhr72.dat
+mv K233-422_mhr72.dat K233-422_mhr7.dat
+
+awk '{print $1*0.02,$2}' K236-421_mhr7.dat > K236-421_mhr72.dat
+mv K236-421_mhr72.dat K236-421_mhr7.dat
+
+awk '{print $1*0.02,$2}' K236-422_mhr7.dat > K236-422_mhr72.dat
+mv K236-422_mhr72.dat K236-422_mhr7.dat
+
+awk '{print $1*0.02,$2}' K240-421_mhr7.dat > K240-421_mhr72.dat
+mv K240-421_mhr72.dat K240-421_mhr7.dat
+
+awk '{print $1*0.02,$2}' K240-422_mhr7.dat > K240-422_mhr72.dat
+mv K240-422_mhr72.dat K240-422_mhr7.dat
+
+awk '{print $1*0.02,$2}' K240-D2_mhr7.dat > K240-D2_mhr72.dat
+mv K240-D2_mhr72.dat K240-D2_mhr7.dat
+
+awk '{print $1*0.02,$2}' K240-D3_mhr7.dat > K240-D3_mhr72.dat
+mv K240-D3_mhr72.dat K240-D3_mhr7.dat
+
+awk '{print $1*0.02,$2}' K242-421_mhr7.dat > K242-421_mhr72.dat
+mv K242-421_mhr72.dat K242-421_mhr7.dat
+
+awk '{print $1*0.02,$2}' K242-422_mhr7.dat > K242-422_mhr72.dat
+mv K242-422_mhr72.dat K242-422_mhr7.dat
+
+awk '{print $1*0.02,$2}' K242-D2_mhr7.dat > K242-D2_mhr72.dat
+mv K242-D2_mhr72.dat K242-D2_mhr7.dat
+
+awk '{print $1*0.02,$2}' K242-D3_mhr7.dat > K242-D3_mhr72.dat
+mv K242-D3_mhr72.dat K242-D3_mhr7.dat
