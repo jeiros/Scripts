4ee495155199f93c19ab55ab754dd2d008cb70c6
Juan Eiros <juaneiros@hotmail.com>
2015-10-22 16:40:39 +0100

    Add a new option for plot type (scatter)

--- AnalysisMDTraj/pca_analysis.py
+++ AnalysisMDTraj/pca_analysis.py
@@ -20,8 +20,9 @@ parser = argparse.ArgumentParser(usage="""{} Trajectories*.nc Topology.prmtop"""
                                  trajectories and their corresponding topology
                                  with MDtraj.Calculate a two-component PCA
                                  based on the pairwise distance between alpha
-                                 carbons of the protein bakcbone. Plot a hexbin
-                                 projection of the trajectories onto the PC space""")
+                                 carbons of the protein bakcbone. Plot a 
+                                 or scatter plot projection of the trajectories
+                                 onto the PC space""")
 
 parser.add_argument("Trajectories", help="""An indefinite amount of AMBER
                     trajectories""", nargs="+")
@@ -29,17 +30,24 @@ parser.add_argument("Trajectories", help="""An indefinite amount of AMBER
 parser.add_argument("Topology", help="""The topology .prmtop file that matches
                     the trajectories""")
 
+parser.add_argument("Plot_type", help="""The type of plot to be used. Can 
+                    be a scatter plot or a hexbin plot.""",
+                    choices=['scatter', 'hexbin'])
+
 parser.add_argument("-s", "--save", help="Save the plots as .png images",
                     action="store_true")
 
 parser.add_argument("-st", "--stride", help="""Stride for the loading of the
                     trajectory. Must be a divisor of the chunk.
-                    Default value is 1.""", default=1)
+                    Default value is 1.""", default=1, type=int)
 
 parser.add_argument("-ch", "--chunk", help="""Number of frames that will be 
                     used by md.iterload to load up the trajectories. Must be
                     a multiplier of the stride. 
-                    Default is 50 frames.""", default=1)
+                    Default is 50 frames.""", default=1, type=int)
+
+parser.add_argument("-t", "--title", help="""Name of the png image where the PCA
+                    plot is stored. Default is PCA.""", default="PCA")
 
 args = parser.parse_args()
 
@@ -58,16 +66,13 @@ def pca_pwise_distance(list_chunks):
     ca_backbone = topology.select("backbone and name CA")
     pair_distances = []
 
-    no_of_pwise_distances = ca_backbone.shape[0]*(ca_backbone.shape[0] -1 )/ 2
-    print("""The number of pairwise distances to be calculated is: %s"""
-          % int(no_of_pwise_distances))
-
     for chunk in list_chunks:
         pairs = topology.select_pairs(ca_backbone, ca_backbone)
         X = md.compute_distances(chunk, pairs)
         pair_distances.append(X)
     distance_array = np.concatenate(pair_distances)
-    print(distance_array.shape)
+    print("Number of data points: %d"% distance_array.shape[0])
+    print("Number of features (pairwise distances): %d" % distance_array.shape[1])
     Y = pca.fit_transform(distance_array)
     return Y
 
@@ -78,19 +83,35 @@ def hex_plot(pca_array):
     plt.xlabel('PC1 (Å)')
     plt.ylabel('PC2 (Å)')
     plt.hexbin(x=PC1, y=PC2, bins='log', mincnt=1)
+    cb = plt.colorbar()
+    cb.set_label('log10(N)')
     if args.save:
-        plt.savefig("PCA.png", dpi=600)
+        plt.savefig(args.title, dpi=600)
     else:
         plt.show()
 
+def scatter_plot(pca_array):
+    PC1 = pca_array[:,0]
+    PC2 = pca_array[:,1]
+    plt.figure()
+    plt.xlabel('PC1 (Å)')
+    plt.ylabel('PC2 (Å)')
+    plt.scatter(x=PC1, y=PC2, marker='x')
+    if args.save:
+        plt.savefig(args.title, dpi=600)
+    else:
+        plt.show()
 
 def main():
     print('\n', args, '\n')
     if args:
         list_chunks = load_Trajs(sorted(args.Trajectories), args.Topology,
-                                 stride=int(args.stride), chunk=int(args.chunk))
+                                 stride=args.stride, chunk=args.chunk)
         pca_array = pca_pwise_distance(list_chunks)
-        hex_plot(pca_array)
+        if args.Plot_type == 'scatter':
+            scatter_plot(pca_array)
+        else:
+            hex_plot(pca_array)
 
 
 if __name__ == "__main__":
