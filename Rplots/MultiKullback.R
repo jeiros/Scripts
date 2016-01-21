b8d9350d6768303d69e112b7103fed423059517c
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-01-21 12:31:31 +0000

    Compares Runs 1 to 7 now

--- Rplots/MultiKullback.R
+++ Rplots/MultiKullback.R
@@ -2,41 +2,42 @@ library(ggplot2)
 library(reshape2)
 
 
+pc1 <- read.table("./KL-PC1_time.dat", quote="\"", skip = 1)
+pc2 <- read.table("./KL-PC2_time.dat", quote="\"", skip = 1)
+pc3 <- read.table("./KL-PC3_time.dat", quote="\"", skip = 1)
 
 
-setwd("/home/je714/Troponin/IAN_Troponin/completehowarthcut/phospho/hmr_runs/try_readdata")
+pc1[pc1 == 0] <- NA
+pc2[pc2 == 0] <- NA
+pc3[pc3 == 0] <- NA
+
+colnames(pc1) <- colnames(pc2) <- colnames(pc3) <- c("Time", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7")
 
 
+d1 <- melt(pc1, id.vars = "Time")
+d2 <- melt(pc2, id.vars = "Time")
+d3 <- melt(pc3, id.vars = "Time")
 
-
-pc1 <- read.table("./KL-PC1.dat", quote="\"")
-pc2 <- read.table("./KL-PC2.dat", quote="\"")
-pc3 <- read.table("./KL-PC3.dat", quote="\"")
-
-
-colnames(pc1) <- colnames(pc2) <- colnames(pc3) <- c("Frame", "1-2", "1-3", "2-3")
-
-
-d1 <- melt(pc1, id.vars = "Frame")
-d2 <- melt(pc2, id.vars = "Frame")
-d3 <- melt(pc3, id.vars = "Frame")
-
-p1 <- ggplot(d1, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("PC1") +
- 	scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1),
+p1 <- ggplot(d1, aes(x = Time, y = value, colour = variable)) + geom_line() + ylab("KLD") +
+  scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1),
   legend.background = element_rect(fill = "white", colour = "black")) + scale_y_continuous(limits=c(0,1)) +
-  scale_x_continuous(limits=c(10000,34000))
+  scale_x_continuous(breaks = seq(0, 750, by = 150)) + ggtitle("PC1") + xlab("Time (ns)") +
+  theme_classic(15)
 
-p2 <- ggplot(d2, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("PC2") +
- 	scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1),
-  legend.background = element_rect(fill = "white", colour = "black")) + scale_y_continuous(limits=c(0,1)) +
-  scale_x_continuous(limits=c(10000,34000))
+p2 <- ggplot(d2, aes(x = Time, y = value, colour = variable)) + geom_line() + ylab("KLD") +
+  scale_color_discrete("WT runs") + theme(legend.position = c(1,1), legend.justification = c(1,1), legend.background = element_rect(fill = "white", colour = "black")) +
+  scale_y_continuous(limits=c(0,1)) +
+  scale_x_continuous(breaks = seq(0, 750, by = 150)) + ggtitle("PC2") + xlab("Time (ns)") +
+  theme_classic(15)
 
-p3 <- ggplot(d3, aes(x = Frame, y = value, colour = variable)) + geom_line() + ylab("PC3") +
- 	scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1),
+p3 <- ggplot(d3, aes(x = Time, y = value, colour = variable)) + geom_line() + ylab("KLD") +
+  scale_color_discrete("") + theme(legend.position = c(1,1), legend.justification = c(1,1),
   legend.background = element_rect(fill = "white", colour = "black")) + scale_y_continuous(limits=c(0,1)) +
-  scale_x_continuous(limits=c(10000,34000))
+  scale_x_continuous(breaks = seq(0, 750, by = 150)) + ggtitle("PC3") + xlab("Time (ns)") +
+  theme_classic(15)
 
 ppi=900
 png("KL-PC_reduced_y_scale.png", width = 8*ppi, height = 8*ppi, res = ppi)
+source("/Users/je714/Scripts/Rplots/Multiple_plot_function.R")
 multiplot(p1,p2,p3)
 dev.off()
