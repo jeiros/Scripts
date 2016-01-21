665c9b6a52f82a0c88fbd899cc15321698bd3ac1
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-01-21 12:26:38 +0000

    Small bash script that uses AWK to multiply the first column of a file by 0.02

---
+++ firstcolumn_multiply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Multiplies the first column of a file by 0.02 to change from
+# frames to ns (to use with HMR data)
+input=$1
+output=$2
+
+awk '{printf($1*0.02"\t\t");for(i=2;i<=NF;++i) printf("%s\t\t", $i); printf("\n")}' ${input} > ${output}
