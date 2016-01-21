5c110731f6cc161479e9e9f19f1cc07b2b160f96
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-01-21 12:29:19 +0000

    Simplified checking of arguments

--- strip_autoimage.sh
+++ strip_autoimage.sh
@@ -10,66 +10,45 @@ sim=$2		  # Can be 000-050, 050-100, 100-150 ...
 cluster=$3        # Can be CTnI_hmr CTnI_runs CTnT_hmr CTnT_runs
 phosphotype=$4    # Can be S1P or SEP
 
-if [[ -z ${run+x} ]]; then
-	printf "Variable %s is missing" ${run}
-	printf "Exiting now... \n\n"
-	exit 1
-else
-	:
-fi
 
-if [[ -z ${sim+x} ]]; then
-	printf "Variable %s is missing" ${sim}
-	printf "Exiting now... \n\n"
+# Check all the inputs are there
+if [[ $# -ne 4 ]]; then
+	printf "Arguments missing\nExiting...\n"
 	exit 1
-else
-	:
-fi
-
-if [[ -z ${cluster+x} ]]; then
-	printf "Variable %s is missing" ${cluster}
-	printf "Exiting now... \n\n"
-	exit 1
-else
-	:
-fi
-
-if [[ -z ${phosphotype+x} ]]; then
-	printf "Variable %s is missing" ${phosphotype}
-	printf "Exiting now... \n\n"
-	exit 1
-else
-	:
 fi
 
 WORKDIR=$PWD
 printf "\nCurrent directory is %s\n" $WORKDIR
+
+mkdir -p ${WORKDIR}/${cluster}/${run}/${phosphotype}/${sim}
 DESTINATION=${WORKDIR}/${cluster}/${run}/${phosphotype}/${sim}
+cd ${DESTINATION}
+scp je714@login.cx1.hpc.ic.ac.uk:/work/je714/phosphoHMR/${cluster}/${run}/${phosphotype}/results/${run}_${cluster}_${sim}.tgz .
 printf "The destination directory is %s\n\n" $DESTINATION
 
-if [ -d "$DESTINATION" ]; then 
-	printf "The directory exists\n\n"
-else
-	printf "The destination directory does not exist\n"
-	printf "\nPlease check the arguments
---------------------------------
-Run:\t\t\t%s\nSim:\t\t\t%s\nCluster:\t\t%s\nPhosphorylation type:\t%s
---------------------------------\n" ${run} ${sim} ${cluster} ${phosphotype}
-	printf "Exiting now...\n\n"
-	exit 1
-fi
+# if [ -d "$DESTINATION" ]; then 
+# 	printf "The directory exists\n\n"
+# else
+# 	printf "The destination directory does not exist\n"
+# 	printf "\nPlease check the arguments
+# --------------------------------
+# Run:\t\t\t%s\nSim:\t\t\t%s\nCluster:\t\t%s\nPhosphorylation type:\t%s
+# --------------------------------\n" ${run} ${sim} ${cluster} ${phosphotype}
+# 	printf "Exiting now...\n\n"
+# 	exit 1
+# fi
 
-hard_drive=/Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/
+# hard_drive=/Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/
 
-if [[ -d  "$hard_drive" ]]; then
-	printf "The hard drive is mounted\n"
-	printf "The destination directory in the hard drive exists\n"
-else
-	printf "The hard drive is not mounted\n"
-	printf "Please mount it and rerun script"
-	printf "Exiting now...\n\n"
-	exit 1
-fi
+# if [[ -d  "$hard_drive" ]]; then
+# 	printf "The hard drive is mounted\n"
+# 	printf "The destination directory in the hard drive exists\n"
+# else
+# 	printf "The hard drive is not mounted\n"
+# 	printf "Please mount it and rerun script"
+# 	printf "Exiting now...\n\n"
+# 	exit 1
+# fi
 
 
 cat << "EOF"
@@ -85,7 +64,6 @@ cat << "EOF"
 EOF
 
 
-cd ${DESTINATION}/
 printf "\n\nUntaring the simulation\nRun:\t\t\t%s\nSim:\t\t\t%s\nCluster:\t\t%s\nPhosphorylation type:\t%s\n" ${run} ${sim} ${cluster} ${phosphotype}
 tar zxvf ${run}_${cluster}_${sim}.tgz
 
@@ -103,8 +81,8 @@ cpptraj <<- EOF
 	run
 EOF
 
-printf "Move the restart file to the Hard Drive"
-scp ${DESTINATION}/*.rst /Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/
+# printf "Move the restart file to the Hard Drive"
+# scp ${DESTINATION}/*.rst /Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/
 
 
 cd ${DESTINATION}/
@@ -112,12 +90,12 @@ printf "\n\nErasing everything that was untarred\n\n"
 ls | grep -v ${run}_${cluster}_${sim}.tgz | xargs rm  #  Erase all files except the tarred trajectory
 
 cd $WORKDIR
-printf "\n\nMoving everything to the hard drive\n\n"
-# Move the tared file inside (and its containing directory) to the hard drive
-mv ${DESTINATION}/ /Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/${sim}/
+# printf "\n\nMoving everything to the hard drive\n\n"
+# # Move the tared file inside (and its containing directory) to the hard drive
+# mv ${DESTINATION}/ /Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/${sim}/
 
-# Copy the stripped and autoimaged trajectory to the hard drive
-scp ./${cluster}/${run}/${phosphotype}/05_Prod_${cluster}_${sim}ns_${run}.nc /Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/
+# # Copy the stripped and autoimaged trajectory to the hard drive
+# scp ./${cluster}/${run}/${phosphotype}/05_Prod_${cluster}_${sim}ns_${run}.nc /Volumes/Seagate_MD/completehowarthcut/phospho/hmr_runs/${cluster}/${run}/${phosphotype}/
 
 cat << "EOF"
 ______  _____  _   _  _____  _ 
