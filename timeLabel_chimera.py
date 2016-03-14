bc73fcb5dcf7c1679310d451b04bce0415d54a76
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-03-14 17:43:12 +0000

    chimera scripts

---
+++ timeLabel_chimera.py
@@ -0,0 +1,6 @@
+# Issue this command inside Chimera
+# 2dlabels create timer text "0 ns" color black ypos .9 xpos .9 size 36
+
+# Issue this per-frame script
+from Chimera import runCommand
+runCommand("2dlabels change timer text '%.1f ns'" % (mdInfo['frame'] *  0.02))
