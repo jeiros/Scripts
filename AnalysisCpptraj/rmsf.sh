cfb8a8b822c764921c72a3ef7f692e90800d7540
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-04-13 17:41:32 +0100

    Better naming for rMSF script

--- AnalysisCpptraj/rmsf.sh
+++ AnalysisCpptraj/rmsf.sh
@@ -2,12 +2,12 @@
 
 
 
-simtime=$1        # Can be 000-050, 000-0500, 100-150 ...
-name=$2        # Can be CTnI_hmr CTnI_runs CTnT_hmr CTnT_runs with _run1 _run2 ...
+
+name=$1        # Can be CTnI_hmr CTnI_runs CTnT_hmr CTnT_runs with _run1 _run2 ...
                                 # Example: CTnI_hmr-run3-S1P
                                 # Example: WT-run3
-prmtop=$3
-trajs=$4
+prmtop=$2
+trajs=$3
 
 
 cpptraj.OMP <<- EOF
@@ -18,6 +18,6 @@ cpptraj.OMP <<- EOF
     run
     # Step 2 - rms fit to average and calculate atomic flucts
     rms ref average_structure @CA,C,O,N,H
-    atomicfluct out rmsf_${name}.${simtime}ns.dat @CA,C,O,N,H byres
+    atomicfluct out rmsf_${name}.dat @CA,C,O,N,H byres
     run
 EOF
