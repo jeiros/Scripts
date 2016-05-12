bd62d7c131ec252504703cf8d77beec80c811031
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-05-12 18:09:01 +0100

    imports in python are case-sensitive

--- timeLabel_chimera.py
+++ timeLabel_chimera.py
@@ -2,5 +2,5 @@
 # 2dlabels create timer text "0 ns" color black ypos .9 xpos .9 size 36
 
 # Issue this per-frame script
-from Chimera import runCommand
+from chimera import runCommand
 runCommand("2dlabels change timer text '%.1f ns'" % (mdInfo['frame']*0.02))
