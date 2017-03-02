52a52eace87768cddba8f1d10a2eb8d0ecadbc14
Juan Eiros <jeirosz@gmail.com>
2017-03-02 15:11:28 +0000

    Improve information

--- min_image.py
+++ min_image.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import mdtraj as md
 import time
 import numpy as np
@@ -7,14 +7,21 @@ from copy import deepcopy
 import itertools
 import sys
 
+TOP = sys.argv[1]
+STRIDE = 10
+TRAJS = sys.argv[2:]
+
+print('Topology is %s' % TOP)
+print('Trajs are {}'.format(list(TRAJS)))
+
 t0 = time.time()
-print("Loading trajectory...")
-T = md.load(sys.argv[2], top=sys.argv[1], stride=10)
-# Glycoprotein trajectory
+T = md.load(TRAJS, top=TOP, stride=STRIDE)
+# Select alpha carbons
 T_gp = T.atom_slice(T.top.select("name CA"))
 # For use at the very end.
 T_gp0 = deepcopy(T_gp)
-print("Done in %.3f seconds" % (time.time() - t0))
+print("%d frames were loaded using a stride of %d (%.1f ns)" % (T.n_frames, STRIDE, ((T.n_frames * T.timestep) / 1000)))
+print("Finished in %.1f seconds" % (time.time() - t0))
 
 t0 = time.time()
 print("Making images...")
@@ -41,7 +48,7 @@ T_gp.unitcell_vectors *= 3
 print("Done in %.3f seconds" % (time.time() - t0))
 
 t0 = time.time()
-print("Measuring distances...")
+print("Measuring distances... (time consuming)")
 min_frames = []
 min_pairs = []
 min_dists = []
@@ -63,5 +70,8 @@ T_copy = deepcopy(T)
 displace = T.unitcell_vectors[:, 0, :] * i + T.unitcell_vectors[:, 1, :] * j + T.unitcell_vectors[:, 2, :] * k
 T_copy.xyz += displace[:, np.newaxis, :]
 T = T.stack(T_copy)
-print("Closest contact found at frame %i, lattice vector %s, atom pair %i-%i, distance %.3f Angstrom" % (min_frames[min_image], str(vecs[min_image]), min_pairs[min_image][0] % n, min_pairs[min_image][1] % n, 10 * min_dists[min_image]))
-T[min_frames[min_image]].save_pdb("%smin_image_f%i_%.1f.pdb" % (sys.argv[2][:-3], min_frames[min_image], 10 * min_dists[min_image]))
+original_frame = min_frames[min_image] * 10 + 1
+print("Closest contact found at frame %i, lattice vector %s, atom pair %i-%i, distance %.3f Angstrom" % (original_frame, str(vecs[min_image]), min_pairs[min_image][0] % n, min_pairs[min_image][1] % n, 10 * min_dists[min_image]))
+pdb_filename = "min_image_f%i_%.1fA_%s.pdb" % (original_frame, 10 * min_dists[min_image], TRAJS[0][:-3])
+print('Saving %s' % pdb_filename)
+T[min_frames[min_image]].save_pdb(pdb_filename)
